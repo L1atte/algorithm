add010f835e80a669ab341e8d40a3f6cd5e145c7
DESKTOP-6OD424I\SIUI <l1atte@qq.com>
2021-09-26 12:09:43 +0800

    no message

--- 206. 反转链表.js
+++ 206. 反转链表.js
@@ -1,10 +1,11 @@
 /*
  * @Author: Latte
- * @Date: 2021-09-24 22:43:48
+ * @Date: 2021-09-26 08:34:08
  * @LAstEditors: Latte
- * @LastEditTime: 2021-09-24 22:43:48
- * @FilePath: \undefinedc:\Users\Latte\Desktop\笔记\反转链表.js
+ * @LastEditTime: 2021-09-26 12:09:26
+ * @FilePath: \algorithm\206. 反转链表.js
  */
+
 let arr = [1, 2, 3, 4, 5];
 
 const reverseList = function (head) {
