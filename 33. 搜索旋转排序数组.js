56a77a0284b8358d61312371245606c271b1567c
L1atte <1454841541@qq.com>
2021-11-29 23:14:15 +0800

    33. 搜索旋转排序数组

---
+++ 33. 搜索旋转排序数组.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Latte
+ * @Date: 2021-11-29 09:07:53
+ * @LAstEditors: Latte
+ * @LastEditTime: 2021-11-29 09:28:09
+ * @FilePath: \algorithm\33. 搜索旋转排序数组.js
+ */
+var search = function (nums, target) {
+	const res = nums.indexOf(target);
+	return res;
+};
