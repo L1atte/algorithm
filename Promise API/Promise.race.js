9d2d111d2ff01f9868e8e615074abfbc4571c9f8
L1atte <1454841541@qq.com>
2022-01-12 22:57:46 +0800

    添加注释

--- Promise API/Promise.race.js
+++ Promise API/Promise.race.js
@@ -10,6 +10,7 @@ function race(promises) {
 		throw Error("Promise.race need length");
 	}
 
+	// 利用 Promise 只能 resolve 或者 reject 一次的特点来完成 Promise.race
 	return new Promise((resolve, reject) => {
 		promises.forEach((item) => {
 			Promise.resolve(item).then(
