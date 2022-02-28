65cd78c882b0f59cd01607334423cd1de07677ce
L1atte <l1atte@qq.com>
2022-02-28 14:03:40 +0800

    155. 最小栈

--- 155. 最小栈.js
+++ 155. 最小栈.js
@@ -2,12 +2,15 @@
  * @Author: Latte
  * @Date: 2022-02-25 08:34:38
  * @LAstEditors: Latte
- * @LastEditTime: 2022-02-25 08:42:40
+ * @LastEditTime: 2022-02-28 14:03:29
  * @FilePath: \algorithm\155. 最小栈.js
  */
+/**
+ * initialize your data structure here.
+ */
 var MinStack = function () {
-	this.stack = [];
-	this.stack.push([]);
+	this.data_stack = [];
+	this.min_stack = [];
 };
 
 /**
@@ -15,36 +18,42 @@ var MinStack = function () {
  * @return {void}
  */
 MinStack.prototype.push = function (val) {
-	this.stack.push([...val]);
+	this.data_stack.push(val);
+	// 如果最小栈为空，或者新元素小于或等于最小栈栈顶，则将新元素压入最小栈
+	if (
+		!this.min_stack.length ||
+		val <= this.min_stack[this.min_stack.length - 1]
+	) {
+		this.min_stack.push(val);
+	}
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-	this.stack.pop();
+	// 如果出栈元素和最小栈栈顶元素值相等，最小栈出栈
+	if (
+		this.data_stack[this.data_stack.length - 1] ==
+		this.min_stack[this.min_stack.length - 1]
+	) {
+		this.min_stack.pop();
+	}
+	return this.data_stack.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function () {
-	if (!this.stack.length) return;
-
-	return this.stack[0];
+	return this.data_stack[this.data_stack.length - 1];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-	if (!this.stack.length) return;
-
-	const tempArr = this.stack.flat(1);
-	tempArr.sort((a, b) => {
-		a - b;
-	});
-	return tempArr[0];
+	return this.min_stack[this.min_stack.length - 1];
 };
 
 /**
