ecf9ff77c8c9b0bd8f7986eab630792c0c10a1fd
L1atte <l4atte@gmail.com>
2022-12-06 08:08:19 +0800

    98. 验证二叉搜索树

--- 98. 验证二叉搜索树.js
+++ 98. 验证二叉搜索树.js
@@ -1,18 +1,15 @@
 var isValidBST = function (root) {
-	let baseVal = -Infinity // 初始化 baseVal 为负无穷
-	let res = true // 结果
-	// traverse函数 同方法1
-	let traverse = function (root) {
-		if (root == null) return
-		traverse(root.left)
-		if (baseVal >= root.val) {
-			res = false
-			return
-		} else {
-			baseVal = root.val
-		}
-		traverse(root.right)
+	// 维护一个 pre，表示“当前遍历节点的前一个节点”，如果前一个节点大于等于当前节点，则表明这棵树的中序遍历过程中出现了乱序的情况，跳出遍历过程、return false
+	let pre = -Infinity
+	const inOrder = (root) => {
+		if (root === null) return true
+		let left = inOrder(root.left)
+
+		if (pre >= root.val) return false
+		pre = root.val
+
+		let right = inOrder(root.right)
+		return left && right
 	}
-	traverse(root)
-	return res
+	return inOrder(root)
 }
