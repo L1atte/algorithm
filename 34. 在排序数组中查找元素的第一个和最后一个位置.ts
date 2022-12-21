9bd002cc8829a8fd3b174c4dbf2a8e448d2305d0
L1atte <l4atte@gmail.com>
2022-12-21 08:13:35 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

---
+++ 34. 在排序数组中查找元素的第一个和最后一个位置.ts
@@ -0,0 +1,42 @@
+const binarySearchLeft = (nums, target) => {
+	let left = -1,
+		right = nums.length
+	while (left + 1 != right) {
+		const mid = Math.floor((left + right) / 2)
+		if (nums[mid] >= target) {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	return right
+}
+
+const binarySearchRight = (nums, target) => {
+	let left = -1,
+		right = nums.length
+	while (left + 1 != right) {
+		const mid = Math.floor((left + right) / 2)
+		if (nums[mid] > target) {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	return left
+}
+
+var searchRange = function (nums, target) {
+	let ans = [-1, -1]
+	const leftIdx = binarySearchLeft(nums, target)
+	const rightIdx = binarySearchRight(nums, target)
+	if (
+		leftIdx <= rightIdx &&
+		rightIdx < nums.length &&
+		nums[leftIdx] === target &&
+		nums[rightIdx] === target
+	) {
+		ans = [leftIdx, rightIdx]
+	}
+	return ans
+}
