f56f9faca35f28e6d63241afd3599e0f7932b868
L1atte <l4atte@gmail.com>
2022-12-22 07:15:40 +0800

    The detail about implement debounce function

--- 手写/debounce.js
+++ 手写/debounce.js
@@ -2,11 +2,12 @@
  * @Author: Latte
  * @Date: 2022-01-21 01:29:53
  * @LAstEditors: Latte
- * @LastEditTime: 2022-01-21 01:32:31
+ * @LastEditTime: 2022-12
  * @FilePath: \algorithm\手写\debounce.js
  */
 /**
  * 防抖
+ * debounce 拿到参数的原理：debounce返回一个函数fn1，去取fn1的参数 args，然后调用原函数Fn的apply方法，即 fn.apply(this, args)
  */
 function debounce(fn, delay = 500) {
 	let timer = null;
