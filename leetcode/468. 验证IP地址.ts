1a6efc93240a3dcfec0283dc7322448dbd082437
L1atte <l4atte@gmail.com>
2023-01-30 08:14:10 +0800

    468. 验证IP地址

---
+++ leetcode/468. 验证IP地址.ts
@@ -0,0 +1,16 @@
+function validIPAddress(queryIP: string): string {
+	const ipv4reg =
+		/^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/;
+	const ipv6reg = /^[0-9a-fA-F]{1,4}(\:[0-9a-fA-F]{1,4}){7}$/;
+
+	if (ipv4reg.test(queryIP)) {
+		return "IPv4";
+	}
+
+	if (ipv6reg.test(queryIP)) {
+		return "IPv6";
+	}
+
+	return "Neither";
+}
+
