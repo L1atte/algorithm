b4a52d38182ba034f80259b749fe41a1fcf32f6f
L1atte <l4atte@gmail.com>
2023-02-03 08:17:41 +0800

    560. 和为 K 的子数组

--- leetcode/560. 和为 K 的子数组.ts
+++ leetcode/560. 和为 K 的子数组.ts
@@ -11,3 +11,22 @@ function subarraySum(nums: number[], k: number): number {
 
 	return count;
 }
+
+function subarraySum1(nums: number[], k: number): number {
+	const mp: Map<number, number> = new Map();
+	mp.set(0, 1);
+	let count = 0,
+		pre = 0;
+	for (const x of nums) {
+		pre += x;
+		if (mp.has(pre - k)) {
+			count += mp.get(pre - k)!;
+		}
+		if (mp.has(pre)) {
+			mp.set(pre, mp.get(pre)! + 1);
+		} else {
+			mp.set(pre, 1);
+		}
+	}
+	return count;
+}
