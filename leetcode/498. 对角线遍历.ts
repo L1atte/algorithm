a3f3942078e997100f3686b487077a920f2e8e43
L1atte <l4atte@gmail.com>
2023-02-10 20:00:03 +0800

    498. 对角线遍历

--- leetcode/498. 对角线遍历.ts
+++ leetcode/498. 对角线遍历.ts
@@ -6,15 +6,13 @@
 function findDiagonalOrder(mat: number[][]): number[] {
 	const m = mat.length;
 	const n = mat[0].length;
-	const res = new Array(m * n).fill(0);
-	let pos = 0;
+	const res: number[] = [];
 	for (let i = 0; i < m + n - 1; i++) {
 		if (i % 2 === 1) {
 			let x = i < n ? 0 : i - n + 1;
 			let y = i < n ? i : n - 1;
 			while (x < m && y >= 0) {
 				res.push(mat[x][y]);
-				pos++;
 				x++;
 				y--;
 			}
@@ -23,7 +21,6 @@ function findDiagonalOrder(mat: number[][]): number[] {
 			let y = i < m ? 0 : i - m + 1;
 			while (x >= 0 && y < n) {
 				res.push(mat[x][y]);
-				pos++;
 				x--;
 				y++;
 			}
