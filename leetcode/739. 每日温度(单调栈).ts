03f9171b9e1983e079a19e96b76766e1f5c2549a
L1atte <l4atte@gmail.com>
2023-02-14 23:22:13 +0800

    739. 每日温度(单调栈)

--- leetcode/739. 每日温度(单调栈).ts
+++ leetcode/739. 每日温度(单调栈).ts
@@ -19,3 +19,21 @@ function dailyTemperatures(temperatures: number[]): number[] {
 
 	return answer;
 }
+
+// 单调栈
+// [73,74,75,71,69,72,76,73]
+// [73, 74, 75, 76]
+function dailyTemperatures1(temperatures: number[]): number[] {
+	const res: number[] = new Array(temperatures.length).fill(0);
+	const stack: number[] = [];
+	for (let i = temperatures.length - 1; i >= 0; i--) {
+		while (stack.length && temperatures[i] >= temperatures[stack[stack.length - 1]]) {
+			stack.pop();
+		}
+		if (stack.length) {
+			res[i] = stack[stack.length - 1] - i;
+		}
+		stack.push(i);
+	}
+	return res;
+}
