f4e1eff2c45a4e4f8b28aaa63b8f85598e19fd06
L1atte <l4atte@gmail.com>
2023-02-22 08:15:53 +0800

    402. 移掉 K 位数字

--- leetcode/402. 移掉 K 位数字.ts
+++ leetcode/402. 移掉 K 位数字.ts
@@ -11,17 +11,21 @@ function removeKdigits(num: string, k: number): string {
 	const stack: string[] = [];
 	for (let i = 0; i < num.length; i++) {
 		const c = num[i];
+		// 只要k>0且当前的c比栈顶的小，则栈顶出栈，k--
 		while (k > 0 && stack.length && stack[stack.length - 1] > c) {
 			stack.pop();
 			k--;
 		}
+		// 当前的字符不是"0"，或栈非空（避免0入空栈），入栈
 		if (c !== "0" || stack.length !== 0) {
 			stack.push(c);
 		}
 	}
+	// 如果还没删够，要从stack继续删，直到k=0
 	while (k > 0) {
 		stack.pop();
 		k--;
 	}
+	// 如果栈空了，返回"0"，如果栈非空，转成字符串返回
 	return stack.length == 0 ? "0" : stack.join("");
 }
