9ecb6eff3b2b6dd25e7dfdd9141ac31ac01ff19d
L1atte <l4atte@gmail.com>
2023-02-27 21:57:54 +0800

    11. 盛最多水的容器

---
+++ leetcode/11. 盛最多水的容器.ts
@@ -0,0 +1,20 @@
+// max(x * y)
+// for(0 ~ length) max = Math.max(max, Math.min(cur , arr[i]) * (i-cur) )
+
+function maxArea(height: number[]): number {
+	let left = 0;
+	let right = height.length - 1;
+	let maxArea = 0; // 最大容积
+	while (left < right) {
+		// 计算出 当前的容积  与最大容积比较，取出最大的
+		const currentArea = (right - left) * Math.min(height[left], height[right]);
+		maxArea = Math.max(maxArea, currentArea);
+		//  left 向内移动
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+	return maxArea;
+}
