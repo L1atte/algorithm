c612f1455e246c48e0c85ace41f4952e2df4f30b
L1atte <l4atte@gmail.com>
2023-03-17 05:37:09 +0800

    17. 电话号码的字母组合

--- leetcode/17. 电话号码的字母组合.ts
+++ leetcode/17. 电话号码的字母组合.ts
@@ -1,4 +1,6 @@
 function letterCombinations(digits: string): string[] {
+  if (digits.length == 0) return [];
+
   const map: Record<string, string> = { '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz' };
   const res: string[] = []
 
