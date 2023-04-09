b94fe11a6398d5322c3eb5bbb712d33f5b5b83e9
L1atte <l4atte@gmail.com>
2023-04-10 06:08:05 +0800

    395. 至少有 K 个重复字符的最长子串

---
+++ leetcode/395. 至少有 K 个重复字符的最长子串.ts
@@ -0,0 +1,28 @@
+// 1. store the count of char
+// 2. get the max length of store
+
+function longestSubstring(s: string, k: number): number {
+	let code = new Map();
+	for (let w of s) {
+		code.set(w, code.has(w) ? code.get(w) + 1 : 1);
+	}
+	let split = "";
+	for (let v of code) {
+		if (v[1] < k) {
+			split = v[0];
+			break;
+		}
+	}
+
+	if (!split) return s.length;
+
+	let maxLen = 0;
+	const other = s.split(split);
+	for (const w of other) {
+		if (w) {
+			let len = longestSubstring(w, k);
+			maxLen = Math.max(maxLen, len);
+		}
+	}
+	return maxLen;
+}
