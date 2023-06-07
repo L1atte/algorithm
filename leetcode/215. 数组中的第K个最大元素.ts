7b2d084e1e0b67aa4f505eac4926b27cbb835e8c
L1atte <l4atte@gmail.com>
2023-06-07 23:47:45 +0800

    215. 数组中的第K个最大元素

---
+++ leetcode/215. 数组中的第K个最大元素.ts
@@ -0,0 +1,37 @@
+// 整个流程就是上浮下沉
+var findKthLargest = function (nums: number[], k: number): number {
+  let heapSize = nums.length;
+  buildMaxHeap(nums, heapSize); // 构建好了一个大顶堆
+  // 进行下沉 大顶堆是最大元素下沉到末尾
+  for (let i = nums.length - 1; i >= nums.length - k + 1; i--) {
+    [nums[0], nums[i]] = [nums[i], nums[0]];
+    --heapSize; // 下沉后的元素不参与到大顶堆的调整
+    // 重新调整大顶堆
+    maxHeapify(nums, 0, heapSize);
+  }
+  return nums[0];
+  // 自下而上构建一颗大顶堆
+  function buildMaxHeap(nums: number[], heapSize: number) {
+    for (let i = Math.floor(heapSize / 2) - 1; i >= 0; i--) {
+      maxHeapify(nums, i, heapSize);
+    }
+  }
+  // 从左向右，自上而下的调整节点
+  function maxHeapify(nums: number[], i: number, heapSize: number) {
+    let l = i * 2 + 1;
+    let r = i * 2 + 2;
+    let largest = i;
+    if (l < heapSize && nums[l] > nums[largest]) {
+      largest = l;
+    }
+    if (r < heapSize && nums[r] > nums[largest]) {
+      largest = r;
+    }
+    if (largest !== i) {
+      // 进行节点调整
+      [nums[i], nums[largest]] = [nums[largest], nums[i]];
+      // 继续调整下面的非叶子节点
+      maxHeapify(nums, largest, heapSize);
+    }
+  }
+};
