549018e98d54974bd356f7b4865307d20b362af9
Latte <l4atte@gmail.com>
2023-07-23 22:55:37 +0800

    update answer

--- leetcode/82. 删除排序链表中的重复元素 II.ts
+++ leetcode/82. 删除排序链表中的重复元素 II.ts
@@ -2,20 +2,22 @@
 // cur.val === next.val
 
 function deleteDuplicates1(head: ListNode | null): ListNode | null {
-  const dummy = new ListNode(0, head);
-  let prev: ListNode | null = dummy;
-  let cur: ListNode | null | undefined = dummy.next;
+  if (!head) {
+    return head;
+  }
 
-  while (cur) {
-    if (cur.next && cur.val === cur.next.val) {
-      const temp = cur.val;
-      while (cur && cur.val === temp) cur = cur.next;
+  const dummy = new ListNode(0, head);
+  let cur: ListNode | null = dummy.next;
+
+  while (cur && cur.next && cur.next.next) {
+    if (cur.next.val === cur.next.next.val) {
+      const temp = cur.next.val;
+      while (cur.next && cur.next.val === temp) {
+        cur.next = cur.next.next;
+      }
     } else {
-      prev.next = cur;
-      prev = cur;
       cur = cur.next;
     }
-    prev.next = cur;
   }
   return dummy.next;
 }
