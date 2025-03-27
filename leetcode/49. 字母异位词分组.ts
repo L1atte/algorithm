2ae3329b6df0af51b36afaaca23fff348770bd95
Latte <l4atte@gmail.com>
2025-03-27 16:58:34 +0800

    update

--- leetcode/49. 字母异位词分组.ts
+++ leetcode/49. 字母异位词分组.ts
@@ -4,11 +4,15 @@ function groupAnagrams(strs: string[]): string[][] {
   const map = new Map<string, Array<string>>();
 
   for (const str of strs) {
+    // 得到一个排序好的字符串
     const s = [...str].sort().join('');
 
+    // 如果哈希表中存在这个模板，那就就将原始字符串 push 进去
     if (map.has(s)) {
       map.get(s)?.push(str);
-    } else {
+    }
+    // 如果不存在，就让这个模板作为键， 数组作为值存入哈希表。
+    else {
       map.set(s, [str]);
     }
   }
