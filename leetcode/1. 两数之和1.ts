2454e5c1f3c0fae568024d65c76ecf4e5f54dbbf
Latte <l4atte@gmail.com>
2026-07-21 14:18:52 +0800

    upodate

--- leetcode/1. 两数之和1.ts
+++ leetcode/1. 两数之和1.ts
@@ -1,10 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
   const len = nums.length;
 
+  if (len === 0) return [];
+
   for (let i = 0; i < len; i++) {
-    const cur = nums[i];
     for (let j = i + 1; j < len; j++) {
-      if (target - cur === nums[j]) return [i, j];
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
   }
 
