1da7a72135d9168308cb79e0ecfe6186160be68d
Latte <l4atte@gmail.com>
2026-07-23 16:25:51 +0800

    update

---
+++ leetcode/226. 翻转二叉树1.ts
@@ -0,0 +1,13 @@
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) return null;
+
+  // 交换左右节点
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
