19d80ceb3edbe90d9d9f9b7bb2a28bda755365fc
Latte <l4atte@gmail.com>
2026-07-30 17:52:27 +0800

    update

---
+++ leetcode/53. 最大子数组和1.ts
@@ -0,0 +1,16 @@
+// [-2,1,-3,4,-1,2,1,-5,4]
+// sum, max
+
+function maxSubArray(nums) {
+  if (!nums || nums.length === 0) return 0;
+
+  let sum = nums[0];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    sum = Math.max(nums[i], sum + nums[i]);
+    max = Math.max(max, sum);
+  }
+
+  return max;
+}
