46c00605e4d58425692236cd951342005e6e3463
Latte <l4atte@gmail.com>
2026-08-02 15:43:18 +0800

    update

---
+++ leetcode/62. 不同路径1.ts
@@ -0,0 +1,19 @@
+// 到 [m,n] = [m-1,n] + [m,n-1]
+// d[1,0] = 1，d[0,1] = 1
+//
+
+function uniquePaths(m: number, n: number): number {
+  const map: number[][] = new Array(m).fill(0).map(() => new Array(n).fill(0));
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || j === 0) {
+        map[i][j] = 1;
+      } else {
+        map[i][j] = map[i - 1][j] + map[i][j - 1];
+      }
+    }
+  }
+
+  return map[m - 1][n - 1];
+}
